b51ea9f64af66f3d4ae9af3e05730baac196bb43
Andrzej Fiedukowicz <fiedukow@gmail.com>
2022-12-17 21:59:59 +0100

    Add basic git setup

--- install.sh
+++ install.sh
@@ -14,6 +14,30 @@ run_safe() {
 	echo $RET_CODE
 }
 
+collect_input() {
+	PI_MUSIC_VARS_FILE="/root/.pimusic.vars"
+
+	if [ -f $PI_MUSIC_VARS_FILE ]; then
+		log_message info "Detected stored input in $PI_MUSIC_VARS_FILE"
+	else
+		echo "Please type your full name:"
+		read PI_MUSIC_FULL_NAME
+		echo "Please type your email:"
+		read PI_MUSIC_EMAIL
+
+		echo "#!/bin/bash" > $PI_MUSIC_VARS_FILE
+		echo "" >> $PI_MUSIC_VARS_FILE
+		echo "export PI_MUSIC_FULL_NAME=\"$PI_MUSIC_FULL_NAME\"" >> $PI_MUSIC_VARS_FILE
+		echo "export PI_MUSIC_EMAIL=\"$PI_MUSIC_EMAIL\"" >> $PI_MUSIC_VARS_FILE
+
+		log_message info "Input stored in '$PI_MUSIC_VARS_FILE'"
+	fi
+
+	source $PI_MUSIC_VARS_FILE
+
+	log_message info "Collected personal info '$PI_MUSIC_FULL_NAME <$PI_MUSIC_EMAIL>'"
+}
+
 verify_root() {
 	if [ $(whoami) != "root" ]; then
 		log_message error "Please run this script as root!"
@@ -53,17 +77,39 @@ enable_ssh_server() {
 	systemctl start ssh
 }
 
+set_up_git_global_config() {
+	log_message info "Setting up basic git config"
+	git config --global user.name "$PI_MUSIC_FULL_NAME"
+	git config --global user.email "$PI_MUSIC_EMAIL"
+	git config --global core.editor vim
+}
+
+generate_ssh_key() {
+	SSH_KEY_LOCATION="/home/music/.ssh/pimusic_ed25519"
+	if [ -f $SSH_KEY_LOCATION ]; then
+		log_message info "Detected exiting SSH key in $SSH_KEY_LOCATION"
+	else
+		log_message info "Setting up ssh key in $SSH_KEY_LOCATION"
+		ssh-keygen -t ed25519 -C "$PI_MUSIC_EMAIL" -q -N "" -f "$SSH_KEY_LOCATION"
+	fi
+
+	log_message info "Your public key is $(cat "$SSH_KEY_LOCATION.pub")"
+}
+
 # Main portion of the script
 mkdir -p logs
 LOG_FILE="logs/piMusic-install.$(date +%Y-%m-%d_%H.%M.%S).log"
 log_message info "Starting installation of piMusic"
 log_message info "Will log to $LOG_FILE"
-
 verify_root
+
+collect_input
 disable_swap
 update_and_upgrade
 install_essentials
 enable_ssh_server
+set_up_git_global_config
+generate_ssh_key
 
 log_message info "Installation complete!"
 
